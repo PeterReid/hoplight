1fb234a2604f241b1a0e932778db54a11a87183c
Peter Reid <peter.d.reid@gmail.com>
2015-08-01 15:05:09 -0400

    Implement Send and LoadData

--- src/vm.rs
+++ src/vm.rs
@@ -20,12 +20,16 @@ pub enum Fault {
 }
 
 pub enum Request {
-    Store{ key: Vec<u8>, value: Vec<u8> },
+    StoreData{ key: Vec<u8>, value: Vec<u8> },
+    LoadData{ key: Vec<u8>, destination_address: u32, destination_length: u32 },
+    Send{ recipient: Vec<u8>, message: Vec<u8> },
 }
 
 mod opcode {
     pub const ADD: u32 = 1;
     pub const STORE_HASH: u32 = 2;
+    pub const LOAD_HASH: u32 = 3;
+    pub const SEND: u32 = 4;
 }
 
 pub type OpResult = Result<Option<Request>, Fault>;
@@ -80,6 +84,7 @@ impl Vm {
         self.pc = self.pc.wrapping_add(amount);
     }
     
+    /// Record CPU cost
     #[inline]
     fn incur_cost(&mut self, cost: u64) {
         self.compute_cost = self.compute_cost.saturating_add(cost);
@@ -96,7 +101,6 @@ impl Vm {
         
         self.advance_pc(4);
         self.incur_cost(1);
-        self.compute_cost += 1;
         
         Ok(None)
     }
@@ -149,9 +153,37 @@ impl Vm {
         hasher.input(&data);
         hasher.result(&mut hash[..]);
         
+        self.advance_pc(3);
         self.incur_cost((byte_length as u64) * 1000);
         
-        Ok(Some(Request::Store{ key: hash, value: data }))
+        Ok(Some(Request::StoreData{ key: hash, value: data }))
+    }
+    
+    fn exec_load_data(&mut self) -> OpResult {
+        let hash_start_word = self.read_pc_memory(1);
+        let dest_start_word = self.read_pc_memory(2);
+        let dest_byte_length = self.read_pc_memory(3);
+        
+        let hash = try!(self.read_memory_bytes(hash_start_word, 64));
+        
+        self.advance_pc(4);
+        self.incur_cost(1);
+        
+        Ok(Some(Request::LoadData{ key: hash, destination_address: dest_start_word, destination_length: dest_byte_length }))
+    }
+    
+    fn exec_send(&mut self) -> OpResult {
+        let recipient_start = self.read_pc_memory(1);
+        let message_start = self.read_pc_memory(2);
+        let message_byte_length = self.read_pc_memory(3);
+        
+        let recipient = try!(self.read_memory_bytes(recipient_start, 32));
+        let message = try!(self.read_memory_bytes(message_start, message_byte_length));
+        
+        self.advance_pc(4);
+        self.incur_cost(30 + (message_byte_length as u64) * 1);
+        
+        Ok(Some(Request::Send{ recipient: recipient, message: message }))
     }
     
     pub fn step(&mut self) -> OpResult {
@@ -159,6 +191,8 @@ impl Vm {
         match opcode {
             opcode::ADD => self.exec_add(),
             opcode::STORE_HASH => self.exec_store_hash(),
+            opcode::LOAD_HASH => self.exec_load_data(),
+            opcode::SEND => self.exec_send(),
             unknown_opcode => Err(Fault::InvalidInstruction(unknown_opcode))
         }
     }
@@ -183,8 +217,7 @@ mod test {
             opcode::STORE_HASH, 3,5,
             0x04030201, 0x00000005]).ok().unwrap();
         match vm.step() {
-            Ok(Some(Request::Store{key, value})) => {
-                println!("{:?} {:?}", key, value);
+            Ok(Some(Request::StoreData{key, value})) => {
                 assert_eq!(value, [1,2,3,4,5].to_vec());
                 assert_eq!(key, [ // b2sum of the value
                     0x6b, 0x4c, 0x45, 0xfb, 0x95, 0x47, 0xe1, 0x9c,
@@ -196,6 +229,26 @@ mod test {
                     0x5f, 0x99, 0x10, 0xb9, 0xbd, 0xd2, 0x07, 0xc6,
                     0x02, 0xe0, 0x6c, 0x30, 0x13, 0x21, 0xa0, 0x23,
                     ].to_vec());
+            }
+            _ => { panic!("Expected a store request") }
+        }
+    }
+    
+    #[test]
+    fn send() {
+        let mut vm = Vm::new(&[
+            opcode::SEND, /* recipient start = */ 6, /*message start = */ 4, /*message bytes = */ 8,
+            0x04030201, 0x08070605, 
+            // recipient
+            0x11111111, 0x22222222, 0x33333333, 0x44444444,
+            0x55555555, 0x66666666, 0x77777777, 0x88888888
+            ]).ok().unwrap();
+        match vm.step() {
+            Ok(Some(Request::Send{recipient, message})) => {
+                assert_eq!(recipient.len(), 32);
+                assert_eq!(recipient[0], 0x11);
+                assert_eq!(recipient[31], 0x88);
+                assert_eq!(message, [1,2,3,4,5,6,7,8].to_vec());
             }
             _ => { panic!("Expected a store request") }
         }
